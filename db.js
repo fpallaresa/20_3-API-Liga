4d8f8eccbed51af4683316389e47276ef7c047bd
Pablo Villarino <pavire182000@gmail.com>
2024-04-20 11:26:50 +0200

    feat: added full.seed.js team prop in player model

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // Cargamos variables de entorno
 require("dotenv").config();
 const DB_CONNECTION = process.env.DB_URL;
-
+const DB_NAME = process.env.DB_NAME;
 const mongoose = require("mongoose");
 
 // Configuración de la conexión
@@ -9,7 +9,7 @@ const config = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000,
-  dbName: "sample-database",
+  dbName: DB_NAME,
 };
 
 const connect = async () => {
@@ -17,6 +17,7 @@ const connect = async () => {
   const name = database.connection.name;
   const host = database.connection.host;
   console.log(`Conectado a la base de datos ${name} en el host ${host}`);
+  return database;
 };
 
 module.exports = { connect };
